4f6012ef68883f9cf268162e039ff1d08bfe4ec9
Barclay SADA <barclay.iversen@sada.com>
2020-05-08 14:07:12 -0700

    Markup for post and function to submit form.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,12 +5,13 @@
     <p class="red">You should only get here if you're authenticated!</p>
     <p v-if="email">Your email address: {{ email }}</p>
     <p>Your Username is : {{ username }}</p>
-   <form>
+   <form @submit.prevent="onSubmit">
 
     <h4>Create a post</h4>
-    <textarea class="newPost" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <input class="postTitle" name="title" v-model="title" placeholder="Title your post">
+    <textarea class="newPost" id="exampleFormControlTextarea1" rows="3" v-model="post"></textarea>
 
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="button">Submit</button>
 </form>
     <div class="posts">
     <h4>Your posts</h4>
@@ -26,6 +27,12 @@
 
 <script>
 export default {
+  data () {
+    return {
+      title: '',
+      post: ''
+    }
+  },
   computed: {
     email () {
       return !this.$store.getters.user ? false : this.$store.getters.user.email
@@ -34,10 +41,23 @@ export default {
       return !this.$store.getters.user
         ? false
         : this.$store.getters.user.username
+    },
+    posts () {
+      return !this.$store.getters.posts ? false : this.$store.getters.posts
+    }
+  },
+  methods: {
+    onSubmit () {
+      const formData = {
+        title: this.title,
+        post: this.post
+      }
+      this.$store.dispatch('newPost', formData)
     }
   },
   created () {
     this.$store.dispatch('fetchUser')
+    this.$store.dispatch('fetchPosts')
   }
 }
 </script>
@@ -48,6 +68,11 @@ export default {
 }
 form {
     display: inline-block;
+}
+
+input {
+  padding: 5px 5px 5px 5px;
+  margin: 5px 5px 5px 0px;
 }
 
 .newPost {
@@ -72,4 +97,14 @@ p {
   border: 3px solid white;
 }
 
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
 </style>
